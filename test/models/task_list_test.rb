b61564ab828def526159f3bf22f3c999d2b9b23c
Kyle McGough <kylemcgough@hey.com>
2025-05-29 10:48:00 -0700

    fixture fix, formatting fix

--- test/models/task_list_test.rb
+++ test/models/task_list_test.rb
@@ -9,14 +9,14 @@ class TaskListTest < ActiveSupport::TestCase
 
   test "should destroy associated tasks when task list is destroyed" do
     task_list = TaskList.create!(name: "Test List")
-    
+
     # Create some tasks for this list
     task1 = Task.create!(title: "Task 1", task_list: task_list)
     task2 = Task.create!(title: "Task 2", task_list: task_list)
-    
+
     # Verify tasks exist
     assert_equal 2, task_list.tasks.count
-    
+
     # Destroy the task list and verify tasks are also destroyed
     assert_difference "Task.count", -2 do
       task_list.destroy
@@ -36,13 +36,13 @@ class TaskListTest < ActiveSupport::TestCase
   test "should persist name correctly" do
     task_list = TaskList.create!(name: "Important Tasks")
     task_list.reload
-    
+
     assert_equal "Important Tasks", task_list.name
   end
 
   test "should handle empty task list" do
     task_list = TaskList.create!(name: "Empty List")
-    
+
     assert_equal 0, task_list.tasks.count
     assert_empty task_list.tasks
   end
@@ -50,7 +50,7 @@ class TaskListTest < ActiveSupport::TestCase
   test "should allow multiple task lists with same name" do
     task_list1 = TaskList.create!(name: "Duplicate Name")
     task_list2 = TaskList.create!(name: "Duplicate Name")
-    
+
     assert task_list1.valid?
     assert task_list2.valid?
     assert_not_equal task_list1.id, task_list2.id
@@ -58,9 +58,9 @@ class TaskListTest < ActiveSupport::TestCase
 
   test "should maintain association when tasks are added" do
     task_list = TaskList.create!(name: "Test List")
-    
+
     task = Task.create!(title: "New Task", task_list: task_list)
-    
+
     assert_includes task_list.tasks, task
     assert_equal task_list, task.task_list
   end
