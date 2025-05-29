501b34a76d262c19a779beb696ee7a451ff01652
Kyle McGough <kylemcgough@hey.com>
2025-05-29 10:14:11 -0700

    link tasks to task lists

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  belongs_to :task_list
   belongs_to :parent, class_name: "Task", optional: true
   has_many :children, class_name: "Task", foreign_key: "parent_id", dependent: :destroy
 
