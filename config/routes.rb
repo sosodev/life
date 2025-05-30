ac992fc91bf89937b018a4038673bd57556c7768
Kyle McGough <kylemcgough@hey.com>
2025-05-30 13:33:35 -0700

    better calendar route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "calendar/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -19,4 +18,6 @@ Rails.application.routes.draw do
     end
   end
   resources :task_lists
+
+  get "/calendar", to: "calendar#index"
 end
