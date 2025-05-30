d6b9c7bd4d043cea09f67e6936b03e3f60235f3f
Kyle McGough <kylemcgough@hey.com>
2025-05-30 11:22:18 -0700

    dashboard link

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root to: "pages#dashboard"
+  root to: "pages#dashboard", as: :dashboard
 
   resources :tasks
 end
