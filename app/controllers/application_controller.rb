2196545942ada799a4c3eea9897726ab1d686182
Kyle McGough <kylemcgough@hey.com>
2025-06-01 00:48:08 -0700

    allow all browsers and add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  http_basic_authenticate_with name: ENV["LIFE_USERNAME"], password: ENV["LIFE_PASSWORD"]
 end
