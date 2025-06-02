3d640a349f54e70aace7f3d3c8d5654d3d141c2c
Kyle McGough <kylemcgough@hey.com>
2025-06-01 21:07:08 -0700

    fix date

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,8 +1,8 @@
 class CalendarController < ApplicationController
   def index
-    @date = params[:date] ? Date.parse(params[:date]) : Date.current
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @scheduled_tasks = Task.includes(:task_list)
-                          .where(scheduled_at: @date.beginning_of_day..@date.end_of_day)
-                          .order(:scheduled_at)
+                           .where(scheduled_at: @date.beginning_of_day..@date.end_of_day)
+                           .order(:scheduled_at)
   end
 end
